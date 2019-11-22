ba482e12fc0981ff6762363387c49c7a10db6070
Robert Vojta <rvojta@me.com>
2019-11-22 13:59:34 +0100

    Remove commented tests
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- anes/tests/parser/mouse/xterm.rs
+++ anes/tests/parser/mouse/xterm.rs
@@ -2,6 +2,8 @@ use anes::{KeyModifiers, Mouse, MouseButton, Sequence};
 
 use crate::test_sequences;
 
+// TODO Cover all cases, especially drag
+
 #[test]
 fn buttons() {
     test_sequences!(
@@ -83,51 +85,3 @@ fn key_modifiers() {
         )),
     );
 }
-
-//#[test]
-//#[cfg(all(feature = "parser", test))]
-//fn special_key_codes() {
-//    test_sequences!(
-//        b"\x1B[1~",
-//        Sequence::Key(KeyCode::Home, KeyModifiers::empty()),
-//        b"\x1B[2~",
-//        Sequence::Key(KeyCode::Insert, KeyModifiers::empty()),
-//        b"\x1B[3~",
-//        Sequence::Key(KeyCode::Delete, KeyModifiers::empty()),
-//        b"\x1B[4~",
-//        Sequence::Key(KeyCode::End, KeyModifiers::empty()),
-//        b"\x1B[5~",
-//        Sequence::Key(KeyCode::PageUp, KeyModifiers::empty()),
-//        b"\x1B[6~",
-//        Sequence::Key(KeyCode::PageDown, KeyModifiers::empty()),
-//        b"\x1B[7~",
-//        Sequence::Key(KeyCode::Home, KeyModifiers::empty()),
-//        b"\x1B[8~",
-//        Sequence::Key(KeyCode::End, KeyModifiers::empty()),
-//        b"\x1B[11~",
-//        Sequence::Key(KeyCode::F(1), KeyModifiers::empty()),
-//        b"\x1B[12~",
-//        Sequence::Key(KeyCode::F(2), KeyModifiers::empty()),
-//        b"\x1B[13~",
-//        Sequence::Key(KeyCode::F(3), KeyModifiers::empty()),
-//        b"\x1B[14~",
-//        Sequence::Key(KeyCode::F(4), KeyModifiers::empty()),
-//        b"\x1B[15~",
-//        Sequence::Key(KeyCode::F(5), KeyModifiers::empty()),
-//        b"\x1B[17~",
-//        Sequence::Key(KeyCode::F(6), KeyModifiers::empty()),
-//        b"\x1B[18~",
-//        Sequence::Key(KeyCode::F(7), KeyModifiers::empty()),
-//        b"\x1B[19~",
-//        Sequence::Key(KeyCode::F(8), KeyModifiers::empty()),
-//        b"\x1B[20~",
-//        Sequence::Key(KeyCode::F(9), KeyModifiers::empty()),
-//        b"\x1B[21~",
-//        Sequence::Key(KeyCode::F(10), KeyModifiers::empty()),
-//        b"\x1B[23~",
-//        Sequence::Key(KeyCode::F(11), KeyModifiers::empty()),
-//        b"\x1B[24~",
-//        Sequence::Key(KeyCode::F(12), KeyModifiers::empty()),
-//    );
-//}
-//
