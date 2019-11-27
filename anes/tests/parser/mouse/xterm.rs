de9296ffebd2b8ecedc83f60e6555fc03a90ce1b
Robert Vojta <rvojta@me.com>
2019-11-27 11:52:39 +0100

    Add more xterm mouse parser tests
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- anes/tests/parser/mouse/xterm.rs
+++ anes/tests/parser/mouse/xterm.rs
@@ -2,10 +2,8 @@ use anes::{KeyModifiers, Mouse, MouseButton, Sequence};
 
 use crate::test_sequences;
 
-// TODO Cover all cases, especially drag
-
 #[test]
-fn buttons() {
+fn button_down() {
     test_sequences!(
         b"\x1B[<0;20;10;M",
         Sequence::Mouse(Mouse::Down(
@@ -28,6 +26,29 @@ fn buttons() {
             10,
             KeyModifiers::empty()
         )),
+    );
+}
+
+#[test]
+fn button_down_with_key_modifiers() {
+    test_sequences!(
+        b"\x1B[<4;20;10;M",
+        Sequence::Mouse(Mouse::Down(MouseButton::Left, 20, 10, KeyModifiers::SHIFT)),
+        b"\x1B[<5;20;10;M",
+        Sequence::Mouse(Mouse::Down(
+            MouseButton::Middle,
+            20,
+            10,
+            KeyModifiers::SHIFT
+        )),
+        b"\x1B[<6;20;10;M",
+        Sequence::Mouse(Mouse::Down(MouseButton::Right, 20, 10, KeyModifiers::SHIFT)),
+    );
+}
+
+#[test]
+fn button_up() {
+    test_sequences!(
         b"\x1B[<0;20;10;m",
         Sequence::Mouse(Mouse::Up(MouseButton::Left, 20, 10, KeyModifiers::empty())),
         b"\x1B[<1;20;10;m",
@@ -39,6 +60,24 @@ fn buttons() {
         )),
         b"\x1B[<2;20;10;m",
         Sequence::Mouse(Mouse::Up(MouseButton::Right, 20, 10, KeyModifiers::empty())),
+    );
+}
+
+#[test]
+fn button_up_with_key_modifiers() {
+    test_sequences!(
+        b"\x1B[<4;20;10;m",
+        Sequence::Mouse(Mouse::Up(MouseButton::Left, 20, 10, KeyModifiers::SHIFT)),
+        b"\x1B[<5;20;10;m",
+        Sequence::Mouse(Mouse::Up(MouseButton::Middle, 20, 10, KeyModifiers::SHIFT)),
+        b"\x1B[<6;20;10;m",
+        Sequence::Mouse(Mouse::Up(MouseButton::Right, 20, 10, KeyModifiers::SHIFT)),
+    );
+}
+
+#[test]
+fn scroll() {
+    test_sequences!(
         b"\x1B[<64;20;10;m",
         Sequence::Mouse(Mouse::ScrollUp(20, 10, KeyModifiers::empty())),
         b"\x1B[<65;20;10;m",
@@ -47,7 +86,61 @@ fn buttons() {
 }
 
 #[test]
-fn key_modifiers() {
+fn scroll_with_key_modifiers() {
+    test_sequences!(
+        b"\x1B[<68;20;10;m",
+        Sequence::Mouse(Mouse::ScrollUp(20, 10, KeyModifiers::SHIFT)),
+        b"\x1B[<69;20;10;m",
+        Sequence::Mouse(Mouse::ScrollDown(20, 10, KeyModifiers::SHIFT)),
+    );
+}
+
+#[test]
+fn drag() {
+    test_sequences!(
+        b"\x1B[<32;20;10;M",
+        Sequence::Mouse(Mouse::Drag(
+            MouseButton::Left,
+            20,
+            10,
+            KeyModifiers::empty()
+        )),
+        b"\x1B[<33;20;10;M",
+        Sequence::Mouse(Mouse::Drag(
+            MouseButton::Middle,
+            20,
+            10,
+            KeyModifiers::empty()
+        )),
+        b"\x1B[<34;20;10;M",
+        Sequence::Mouse(Mouse::Drag(
+            MouseButton::Right,
+            20,
+            10,
+            KeyModifiers::empty()
+        )),
+    );
+}
+
+#[test]
+fn drag_with_key_modifiers() {
+    test_sequences!(
+        b"\x1B[<36;20;10;M",
+        Sequence::Mouse(Mouse::Drag(MouseButton::Left, 20, 10, KeyModifiers::SHIFT,)),
+        b"\x1B[<37;20;10;M",
+        Sequence::Mouse(Mouse::Drag(
+            MouseButton::Middle,
+            20,
+            10,
+            KeyModifiers::SHIFT,
+        )),
+        b"\x1B[<38;20;10;M",
+        Sequence::Mouse(Mouse::Drag(MouseButton::Right, 20, 10, KeyModifiers::SHIFT,)),
+    );
+}
+
+#[test]
+fn key_modifier_combinations() {
     test_sequences!(
         b"\x1B[<4;20;10;m",
         Sequence::Mouse(Mouse::Up(MouseButton::Left, 20, 10, KeyModifiers::SHIFT)),
